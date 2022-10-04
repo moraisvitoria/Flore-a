b1f3daf71049513639aa96b877aff0a08f1b3825
Driely Santos <santosdriely98@gmail.com>
2022-10-04 16:50:34 -0300

    Reajustes background

--- script.js
+++ script.js
@@ -1,80 +1,26 @@
-/**
- * JavaScript do aplicativo
- * Depende de "jQuery" (https://jquery.com/)
- *
- * OBS1: Este é o aplicativo principal, para que o tema (template) do site
- * opere. Posteriormente, quando necessário, cada página (conteúdo) terá seu
- * próprio JavaScript, assim, somente o JavaScript necessário será carregado.
- * 
- * OBS1: Todas as funções que iniciam com um cifrão ($) fazem parte da 
- * biblioteca jQuery, ou seja, não são do JavaScript "puro" (vanilla).
- *
- * Para saber mais:
- *   • https://www.w3schools.com/js/
- *   • https://www.w3schools.com/jsref/
- *   • https://www.w3schools.com/jquery/
- */
-
-/**
- * runApp() → Aplicativo principal
- * Este aplicativo é executado pela última linha deste código.
- */
 function runApp() {
-
-  // Carrega a página inicial do site quando este iniciar:
   loadPage('home');
-
-  /**
-   * jQuery → Quando houver click em um elemento <a>, execute o aplicativo 
-   * "routerLink":
-   **/
-   // $('a').click(routerLink);
    $(document).on('click', 'a', routerLink);
 
 }
 
-// Prepara o menu dropdown para exibição correta conforme a largura da tela:
 resize();
 
-// Se a largura da tela mudar durante a execução, reajusta o menu dropdown:
 $(window).resize(resize);
 
-/**
- * routerLink() → Aplicativo que processa cliques nos elementos <a>:
- */
 function routerLink() {
    
-  // Quando clica em qualquer link, oculta o menu dropdown:
   hideMenu();
 
-  /**
-   * jQuery → Recebe o atributo "href" do link clicado e armazena em 'href':
-   * A função "$(this)" faz referência ao elemento que foi clicado e disparou 
-   * este processo.
-   **/
   var href = $(this).attr('href');
 
-  // Se o link clicado é o botão do menu (href="menu")...
   if (href == 'menu') {
 
-    // Chama a função que controla a exibição do menu dropdown:
     toggleMenu();
 
-    // Sai de "routerLink()" sem fazer mais nada (false):
     return false;
   }
 
-  /**
-   * Se faz referência a link externo que começa com "http://" OU "https://",
-   * ou faz referência a uma âncora que começa com "#"...
-   * 
-   * OBS: O código "||" (pipe pipe) significa o "OU" (OR) lógico em JavaScript.
-   * 
-   * Referências: 
-   *    https://youtu.be/mp3g9IQ651g
-   *    https://www.w3schools.com/jsref/jsref_substring.asp
-   *    https://www.w3schools.com/js/js_if_else.asp
-   **/
   if (
     // Se clicou em um link que começa com "http://...", OU
     href.substr(0, 7) == 'http://' ||
@@ -86,23 +32,11 @@ function routerLink() {
     href.substr(0, 1) == '#'
   ) {
 
-    /**
-     * Encerra o programa retornando "true" (verdade) para que o HTML abra o 
-     * link normalmente:
-     **/
     return true;
   }
 
-  /**
-   * Se clicou em um link interno (rota para uma página), executa o programa 
-   * "loadPage()" que carrega a página correta:
-   **/
   loadPage(href);
 
-  /**
-   * Encerra o programa retornando "false" (falso) para que a ação do HTML ao
-   * clicar no link seja bloqueada:
-   **/
   return false;
 }
 
@@ -138,11 +72,6 @@ function loadPage(href) {
 
 }
 
-/**
- * setTitle() → Aplicativo que troca o <title> do documento conforme cada 
- * página é acessada. Para isso, inclua a chamada "setTitle('Titulo')" em cada
- * arquivo "script.js" de cada página.
- */
 function setTitle(title = '') {
 
   // Se não definiu um valor para title...
@@ -198,40 +127,19 @@ function toggleMenu() {
 
     // Chama a função que oculta o menu:
     hideMenu();
-
-    // Se não...
   } else {
-
-    // Chama a função que mostra o menu:
     showMenu();
   }
 }
 
-/**
- * hideMenu() → Aplicativo que oculta o menu dropdown e também aplica o efeito
- * de animação no ícone do botão de menu. A classe "fa-rotate-90" que gira o 
- * ícone, faz parte da biblioteca "Font Awesome". Referências:
- *     https://fontawesome.com/docs/web/style/rotate
- */
 function hideMenu() {
-
-  // jQuery → Oculta o menu:
   $('#dropable').hide('fast');
-
-  // jQuery → Remove rotação do ícone do botão do menu:
   $('#btnMenu i').removeClass('fa-rotate-90');
 }
 
-/**
- * showMenu() → Aplicativo que mostra o menu dropdown e também aplica o efeito
- * de animação no ícone do botão de menu. 
- */
+
 function showMenu() {
-
-  // jQuery → Mostra o menu:
   $('#dropable').show('fast');
-
-  // jQuery → Rotaciona o ícone do botão do menu:
   $('#btnMenu i').addClass('fa-rotate-90');
 }
 
@@ -264,9 +172,6 @@ function getCookie(cname) {
   return "";
 }
 
-/**
- * Cria o menu de navegação complementar de sobre:
- */
 var aboutMenu = `
 <a href="site"><i class="fa-solid fa-globe fa-fw"></i><span>Sobre o site</span></a>
 <a href="team"><i class="fa-solid fa-users fa-fw"></i><span>Quem somos</span></a>
@@ -274,5 +179,4 @@ var aboutMenu = `
 <a href="contacts"><i class="fa-solid fa-comments fa-fw"></i><span>Contatos</span></a>
 `;
 
-// jQuery → Executa aplicativo "runApp" quando o documento estiver pronto:
 $(document).ready(runApp);
